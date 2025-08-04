8032e254a5bea9fcd885406b926c6e1549611544
Md.Sami-El Bashar Rhythm <md.sami-elbasharrhythm@MdSami-Els-MacBook-Air-2.local>
2025-08-05 04:37:29 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const splash = document.querySelector('.splash-screen');
+  
+    setTimeout(() => {
+      splash.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+      splash.style.opacity = '0';
+      splash.style.transform = 'scale(1.05)';
+  
+      setTimeout(() => {
+        window.location.href = "welcome.html"; 
+      }, 500);
+    }, 400);
+  });
+  
